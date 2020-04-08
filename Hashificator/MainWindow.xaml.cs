d8c82af62a362e93855555a51da0f6c94350f996
ashakoor <60638768+ashakoor@users.noreply.github.com>
2020-04-08 12:42:36 -0400

    rename project, switch to .NET 5.0 preview

--- Hashificator/MainWindow.xaml.cs
+++ Hashificator/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interop;
 
-namespace HashCalc
+namespace Hashificator
 {
     public partial class MainWindow : Window
     {
